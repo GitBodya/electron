4bde9abecff06214426e97a1ffee0c2fcd9425f6
Bohdanluzhetskyi <bluzhetskyi@netfully.com>
2023-05-23 21:01:17 +0300

    refactor: replace code using handleSetTitle function

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
+function handleSetTitle(event, title) {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    win.setTitle(title)
+}
+
 function createWindow() {
     const mainWindow = new BrowserWindow({
         webPreferences: {
@@ -8,17 +14,12 @@ function createWindow() {
         },
     })
 
-    ipcMain.on('set-title', (event, title) => {
-        const webContents = event.sender
-        const win = BrowserWindow.fromWebContents(webContents)
-        win.setTitle(title)
-    })
-
     mainWindow.loadFile('index.html')
 }
 
 
 app.whenReady().then(() => {
+    ipcMain.on('set-title', handleSetTitle)
     createWindow()
     app.on('activate', function() {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
